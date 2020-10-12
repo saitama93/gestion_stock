9e511b55d8653607f9943bf4ece94d237feb80f3
saitama93 <igalilmi32@gmail.com>
2020-10-12 13:41:33 +0000

    Ajout des repository

---
+++ Site/src/Repository/MaterielRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Materiel;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Materiel|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Materiel|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Materiel[]    findAll()
+ * @method Materiel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MaterielRepository extends ServiceEntityRepository
+{
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Materiel::class);
+    }
+
+
+    // /**
+    //  * @return Materiel[] Returns an array of Materiel objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Role
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
---
+++ Site/src/Repository/SpecificiteRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Specificite;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Specificite|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Specificite|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Specificite[]    findAll()
+ * @method Specificite[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SpecificiteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Specificite::class);
+    }
+
+    // /**
+    //  * @return Specificite[] Returns an array of Specificite objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Specificite
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
