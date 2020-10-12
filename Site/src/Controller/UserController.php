2fa85dc2b41ca0095453d153d09a458c99d95773
saitama93 <igalilmi32@gmail.com>
2020-10-12 07:03:40 +0000

    Mise en place de Dompdf et Swiftmailer pour envoie de mail avec pdf lors de la création d'un utilisateur

--- Site/src/Controller/UserController.php
+++ Site/src/Controller/UserController.php
@@ -3,17 +3,13 @@
 namespace App\Controller;
 
 use Mpdf\Mpdf;
+use Dompdf\Dompdf;
 use App\Entity\User;
 use App\Form\UserType;
 use PHPMailer\PHPMailer\PHPMailer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -24,10 +20,11 @@ class UserController extends AbstractController
      * @Route("/user", name="User.index",methods={"GET"})
      * Permet d'afficher tous les utilisateurs
      */
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $em = $this->getDoctrine()->getManager();
         $users = $em->getRepository(User::class)->findBy(array('present' => 1));
-        return $this->render('user/index.html.twig',[
+        return $this->render('user/index.html.twig', [
             'users' => $users,
         ]);
     }
@@ -36,29 +33,30 @@ class UserController extends AbstractController
      * @Route("/user/add",name="User.add",methods={"GET","POST"})
      * Permet de créer un utilisateur
      */
-    public function add(Request $request, UserPasswordEncoderInterface $passwordEncoder){
+    public function add(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer, EntityManagerInterface $em)
+    {
         $message = '';
         $user = new User();
-       
-       $form =  $this->createForm(UserType::class, $user);
+
+        $form =  $this->createForm(UserType::class, $user);
 
         if ($request->isMethod('POST')) {
             $form->handleRequest($request);
-            if ($form->isValid()){
-                ini_set("SMTP","relais-exchange.doubs.fr");
+            if ($form->isValid()) {
+                ini_set("SMTP", "relais-exchange.doubs.fr");
                 try {
                     $plainPasswd = $user->getPassword();
                     $user->setPlainPassword($plainPasswd);
-                    $password = $passwordEncoder->encodePassword($user,$plainPasswd);
+                    $password = $passwordEncoder->encodePassword($user, $plainPasswd);
                     $user->setPassword($password);
-                    switch ($_POST['roles']){
+                    switch ($_POST['roles']) {
                         case 0:
                             $user->setRoles(array());
                             break;
-                        case 1 :
+                        case 1:
                             $user->setRoles(array('ROLE_ADMIN'));
                             break;
-                        case 2 :
+                        case 2:
                             $user->setRoles(array('ROLE_PUBLIC'));
                             break;
                         default:
@@ -66,45 +64,58 @@ class UserController extends AbstractController
                             break;
                     }
                     $user->setPresent(1);
-                    $em = $this->getDoctrine()->getManager();
                     $em->persist($user);
                     $em->flush();
-                    $this->addFlash('success', 'Compte créé.');
+                    $this->addFlash('success', 'Compte créé et un mail vous a été envoyé avec vos identifiants');
                     $message = 'Voici vos informations utilisateurs afin d\'accéder à l\'application';
-                    $mpdf = new Mpdf();
-                    $contenus = '<h1>Informations de l\'utilisateur</h1>'
-                        . '<p>Nom : '.$user->getNom().'</p>'
-                        . '<p>Prenom : '.$user->getPrenom().'</p>'
-                        . '<p>Login : '.$user->getUsername().'</p>'
-                        . '<p>Mot de passe : '.$user->getPlainPassword().'</p>';
-                    $mpdf->WriteHTML(utf8_encode($contenus));
-                    $pdf = $mpdf->Output('User_info.pdf','S');
-                    $email = new PHPMailer();
-                    $email->setFrom('stockA2SR@doubs.fr'); //@todo modifier mail départ
-                    $email->Subject=utf8_decode('User créé');
-                    $email->Body=utf8_decode($message);
-                    if ($_ENV['APP_ENV'] == 'dev' || $_ENV['APP_ENV'] == 'test'){
-                        $email->addAddress('igalilmi32-148be1@inbox.mailtrap.io');
-                    } else {
-                        $email->addAddress($user->getMail());
-                    }
-                    $email->addStringAttachment($pdf,'User_info.pdf');
 
-                    $email->send();
+
+                    // Instanciation de la class Dompdf et création du fichier pdf
+                    $dompdf = new Dompdf();
+                    $html = $this->render('pdf/userInfo.html.twig', [
+                        'nom' => $user->getNom(),
+                        'prenom' => $user->getPrenom(),
+                        'pseudo' =>  $user->getUsername(),
+                        'plainPassword' => $user->getPlainPassword(),
+                        'mail' => $user->getMail()
+                    ]);
+
+                    $dompdf->loadHtml($html->getContent());
+                    $dompdf->setPaper('A4', 'landscape');
+                    $dompdf->output();
+                    $dompdf->render();
+                    $dompdf->stream('infos_' . $user->getNom());
+
+                    try {
+                        $transport = (new \Swift_SmtpTransport('relais-exchange.doubs.fr', 25));
+
+                        // Création du mail
+                        $mailer = new \Swift_Mailer($transport);
+
+                        $mail = (new \Swift_Message('Wonderful Subject'))
+                            ->setFrom(['rononoa.zoro@mugiwara.fr' => 'RONONOA Zoro'])
+                            ->setTo(['igal.ilmiamir@doubs.fr'])
+                            ->setBody('Voici vos informations utilisateurs afin d\'accéder à l\'application');
+
+                        // Envoie du mail
+                        $mailer->send($mail);
+
+                    } catch (\Exception $e) {
+                        echo 'Exception reçue : ',  $e->getMessage(), "\n";
+                    }
+
                     return $this->redirectToRoute('User.index');
-                } catch (UniqueConstraintViolationException $exception){
+                } catch (UniqueConstraintViolationException $exception) {
                     $message = 'Cet utilisateur existe déjà !';
                 }
             }
-            if($form->isSubmitted()){
-               
+            if ($form->isValid() && $form->isSubmitted()) {
+
                 $this->addFlash('success', 'Utilisateur créé.');
-
             }
-
         }
-        return $this->render('user/addUser.html.twig',[
-            'message'=>$message,
+        return $this->render('user/addUser.html.twig', [
+            'message' => $message,
             'form' => $form->createView(),
         ]);
     }
@@ -113,7 +124,8 @@ class UserController extends AbstractController
      * @Route("user/edit/{id}",name="User.edit",methods={"GET","POST"})
      * Permet de modifier les utilisateurs
      */
-    public function edit(Request $request, $id,UserPasswordEncoderInterface $passwordEncoder){
+    public function edit(Request $request, $id, UserPasswordEncoderInterface $passwordEncoder)
+    {
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository(User::class)->find($id);
         $check = '';
@@ -122,22 +134,22 @@ class UserController extends AbstractController
         $plainPasswd = $user->getPassword();
         if ($request->isMethod('POST')) {
             $form->handleRequest($request);
-            if ($form->isValid()){
-                ini_set("SMTP","relais-exchange.doubs.fr");
+            if ($form->isValid()) {
+                ini_set("SMTP", "relais-exchange.doubs.fr");
                 try {
                     $plainPasswd = $user->getPassword();
                     $user->setPlainPassword($plainPasswd);
-                    $password = $passwordEncoder->encodePassword($user,$plainPasswd);
+                    $password = $passwordEncoder->encodePassword($user, $plainPasswd);
                     $user->setPassword($password);
-                    if ($user->getIduser() != 1 && $user->getIduser() != 2){
-                        switch ($_POST['roles']){
+                    if ($user->getIduser() != 1 && $user->getIduser() != 2) {
+                        switch ($_POST['roles']) {
                             case 0:
                                 $user->setRoles(array());
                                 break;
-                            case 1 :
+                            case 1:
                                 $user->setRoles(array('ROLE_ADMIN'));
                                 break;
-                            case 2 :
+                            case 2:
                                 $user->setRoles(array('ROLE_PUBLIC'));
                                 break;
                             default:
@@ -154,31 +166,35 @@ class UserController extends AbstractController
                     $message = 'Voici vos informations utilisateurs afin d\'accéder à l\'application';
                     $mpdf = new Mpdf();
                     $contenus = '<h1>Informations de l\'utilisateur</h1>'
-                        . '<p>Nom : '.$user->getNom().'</p>'
-                        . '<p>Prenom : '.$user->getPrenom().'</p>'
-                        . '<p>Login : '.$user->getUsername().'</p>'
-                        . '<p>Mot de passe : '.$user->getPlainPassword().'</p>';
+                        . '<p>Nom : ' . $user->getNom() . '</p>'
+                        . '<p>Prenom : ' . $user->getPrenom() . '</p>'
+                        . '<p>Login : ' . $user->getUsername() . '</p>'
+                        . '<p>Mot de passe : ' . $user->getPlainPassword() . '</p>';
                     $mpdf->WriteHTML(utf8_encode($contenus));
-                    $pdf = $mpdf->Output('User_info.pdf','S');
-                    $email = new PHPMailer();
+                    $pdf = $mpdf->Output('User_info.pdf', 'S');
+                    $email = new PHPMailer(true);
                     $email->setFrom('stockA2SR@doubs.fr'); //@todo modifier mail départ
-                    $email->Subject=utf8_decode('User modifié');
-                    $email->Body=utf8_decode($message);
-                    if ($_ENV['APP_ENV'] == 'dev' || $_ENV['APP_ENV'] == 'test'){
-                        $email->addAddress('igalilmi32-148be1@inbox.mailtrap.io');
+                    $email->Subject = utf8_decode('User modifié');
+                    $email->Body = utf8_decode($message);
+                    if ($_ENV['APP_ENV'] == 'dev' || $_ENV['APP_ENV'] == 'test') {
+                        $email->addAddress('igal.ilmiamir@doubs.fr');
                     } else {
                         $email->addAddress($user->getMail());
                     }
-                    $email->addStringAttachment($pdf,'User_info.pdf');
-
-                    $email->send();
+                    $email->addStringAttachment($pdf, 'User_info.pdf');
+                    // dd($email);
+                    try {
+                        $email->send();
+                    } catch (\Exception $e) {
+                        echo 'Exception reçue : ',  $e->getMessage(), "\n";
+                    }
                     return $this->redirectToRoute('User.index');
-                } catch (UniqueConstraintViolationException $exception){
+                } catch (UniqueConstraintViolationException $exception) {
                     $message = 'Cet utilisateur existe déjà !';
                 }
             }
         }
-        return $this->render('user/editUser.html.twig',[
+        return $this->render('user/editUser.html.twig', [
             'id' => $id,
             'user' => $user,
             'check' => $check,
@@ -190,43 +206,47 @@ class UserController extends AbstractController
      * @Route("user/delete/{id}",name="User.delete",methods={"GET","POST"})
      * Permet de supprimer un utilisateur avec page de confirmation
      */
-    public function delete(Request $request,$id){
+    public function delete(Request $request, $id)
+    {
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository(User::class)->find($id);
-        if ($id == 1 || $id == 2){
+        if ($id == 1 || $id == 2) {
             return $this->redirectToRoute('User.index');
         }
-        if ($request->isMethod('POST')){
+        if ($request->isMethod('POST')) {
             $user->setPresent(0);
             $em->persist($user);
             $em->flush();
             return $this->redirectToRoute('User.index');
         }
-        return $this->render('user/deleteUser.html.twig',
+        return $this->render(
+            'user/deleteUser.html.twig',
             [
                 'user' => $user
-            ]);
+            ]
+        );
     }
 
     /**
      * @Route("user/exportCsv",name="User.exportCsv",methods={"GET","POST"})
      * Permet d'exporter tous les utilisateurs sous format CSV
      */
-    public function exportCsv(){
+    public function exportCsv()
+    {
         $repository = $this->getDoctrine()->getRepository(User::class);
         $users = $repository->findAll();
-        $chemin='./csv/user.csv';
-        $fp = fopen($chemin,'w');
-        fputs($fp, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
-        fputcsv($fp,array(
+        $chemin = './csv/user.csv';
+        $fp = fopen($chemin, 'w');
+        fputs($fp, $bom = (chr(0xEF) . chr(0xBB) . chr(0xBF)));
+        fputcsv($fp, array(
             'ID',
             'Nom',
             'Prenom',
             'Mail',
             'Roles'
-        ),';','"');
-        foreach ($users as $user){
-            if (sizeof($user->getRoles())==2) $roles = 'Admin';
+        ), ';', '"');
+        foreach ($users as $user) {
+            if (sizeof($user->getRoles()) == 2) $roles = 'Admin';
             else $roles = 'User';
             $data = array(
                 $user->getIduser(),
@@ -235,13 +255,13 @@ class UserController extends AbstractController
                 $user->getMail(),
                 $roles,
             );
-            fputcsv($fp,$data,';','"');
+            fputcsv($fp, $data, ';', '"');
         }
         fclose($fp);
 
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($chemin).'"');
+        header('Content-Disposition: attachment; filename="' . basename($chemin) . '"');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
@@ -254,24 +274,25 @@ class UserController extends AbstractController
      * @Route("/user/downloadModel",name="User.downloadModel",methods={"GET"})
      * Permet de télécharger le modèle du CSV
      */
-    public function downloadModel(){
+    public function downloadModel()
+    {
         $repository = $this->getDoctrine()->getRepository(User::class);
         $users = $repository->findAll();
-        $chemin='./csv/user.csv';
+        $chemin = './csv/user.csv';
 
-        $fp = fopen($chemin,'w');
-        fputs($fp, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
-        fputcsv($fp,array(
+        $fp = fopen($chemin, 'w');
+        fputs($fp, $bom = (chr(0xEF) . chr(0xBB) . chr(0xBF)));
+        fputcsv($fp, array(
             'ID',
             'Nom',
             'Prenom',
             'Mail',
             'Roles'
-        ),';','"');
+        ), ';', '"');
 
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($chemin).'"');
+        header('Content-Disposition: attachment; filename="' . basename($chemin) . '"');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
@@ -284,7 +305,8 @@ class UserController extends AbstractController
      * @Route("user/importCSV",name="User.importCSV",methods={"GET","POST"})
      * Permet de rajouter de nouveau User via un import d'un CSV
      */
-    public function importCsv(Request $request){
+    public function importCsv(Request $request)
+    {
         $em = $this->getDoctrine()->getManager();
         if ($request->isMethod('POST')) {
             // @codeCoverageIgnoreStart
@@ -292,16 +314,16 @@ class UserController extends AbstractController
                 $extension = array('.file');
                 $file = strrchr($_FILES['csvFile']['name'], '.');
                 if (in_array($file, $extension)) {
-                    $csv = fopen($_FILES['csvFile']['tmp_name'],'r');
-                    while (!feof($csv)){
-                        $line[]= fgetcsv($csv,1024,';');
+                    $csv = fopen($_FILES['csvFile']['tmp_name'], 'r');
+                    while (!feof($csv)) {
+                        $line[] = fgetcsv($csv, 1024, ';');
                     }
                     fclose($csv);
-                    $verif = false ;
-                    foreach ($line as $data){
-                        if ($verif){
-                            $user = $em->getRepository(User::class)->findOneBy(array('iduser'=>$data[0]));
-                            if ($user==null){
+                    $verif = false;
+                    foreach ($line as $data) {
+                        if ($verif) {
+                            $user = $em->getRepository(User::class)->findOneBy(array('iduser' => $data[0]));
+                            if ($user == null) {
                                 $roles = [];
                                 if ($data[4] == 'Admin') $roles = array('ROLE_ADMIN');
                                 $user = new User();
@@ -314,7 +336,7 @@ class UserController extends AbstractController
                                 $em->flush();
                             }
                         }
-                        $verif = true ;
+                        $verif = true;
                     }
                     return $this->redirectToRoute('User.index');
                 }
@@ -323,4 +345,31 @@ class UserController extends AbstractController
         }
         return $this->render('user/importCSV.html.twig');
     }
+
+
+    // /**
+    //  * @Route("/testMail", name ="User.testMail")
+    //  */
+    // public function testMail(\Swift_Mailer $mailer)
+    // {
+
+    //     try {
+    //         $transport = (new \Swift_SmtpTransport('relais-exchange.doubs.fr', 25));
+
+    //     // Create the Mailer using your created Transport
+    //     $mailer = new \Swift_Mailer($transport);
+
+    //     $message = (new \Swift_Message('Wonderful Subject'))
+    //         ->setFrom(['rononoa.zoro@mugiwara.fr' => 'RONONOA Zoro'])
+    //         ->setTo(['igal.ilmiamir@doubs.fr'])
+    //         ->setBody('Here is the message itself');
+
+    //     // Send the message
+    //     $result = $mailer->send($message);
+    //     }  catch (\Exception $e) {
+    //         echo 'Exception reçue : ',  $e->getMessage(), "\n";
+    //     }
+
+    //     dd("Mails sended");
+    // }
 }
